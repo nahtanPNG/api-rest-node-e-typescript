bac55dfd165225e71137daff10274c68e21de03d
nahtanpng <nathan.ferreiira13@gmail.com>
2024-01-06 20:01:59 -0300

    feat: Initial config

---
+++ src/server/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const server = express(); //Instância do servidor
+
+//Configurações do servidor
+server.get('/', (_, res) => {
+    return res.send('Olá, DEV!'); //Retorna uma resposta
+});
+
+
+export { server };
