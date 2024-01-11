1c1678660e7934d65784176927fa96643d62d266
nahtanPNG <nathan.ferreiira13@gmail.com>
2024-01-11 15:41:46 -0300

    feat: Validation

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,13 +1,40 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
-interface ICidade{ //Definindo Tipagem
+interface ICidade { //Definindo Tipagem
   nome: string;
+  estado: string;
 }
 
-//Controller de criação de cidades
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => { //Definindo a tipagem do body
+//Validação dos atributos com o yup
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
+});
 
-  console.log(req.body.nome);
+//Controller de criação de cidades
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => { //Definindo a tipagem do body
+
+  let validatedData: ICidade | undefined = undefined;
+
+  try { //Recebendo os dados e validando
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+  } catch (erro) { //Tratamento de erros
+    const yupError = erro as yup.ValidationError;
+    const errors: Record<string, string> = {}; //Objeto vazio
+
+    //Mapeando erros
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message; //Adicionando no Record que estava vazio
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors
+    });
+  }
+  console.log(validatedData);
 
   return res.send('Create!');
 };
